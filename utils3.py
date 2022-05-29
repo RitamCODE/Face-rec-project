18d0cd084dcfa819e0388057d10bd85d1406d12a
RitamCODE <82175279+RitamCODE@users.noreply.github.com>
2022-05-29 23:26:08 +0530

    Update utils3.py

--- utils3.py
+++ utils3.py
@@ -3,6 +3,7 @@
 import numpy as np
 import cv2
 
+# haar cascade classifier
 haar = cv2.CascadeClassifier('./app/data/haarcascade_frontalface_default.xml')
 
 
@@ -23,8 +24,7 @@ def pipeline_model3():
     while True:
         ret, frame = cap.read()
         # ret will return true if the video can be read
-        # frame is our single image
-        # if ret == False:
+        # frame is our single imag
 
         frame = face_detect(frame)
         if ret:
@@ -34,7 +34,7 @@ def pipeline_model3():
         else:
             break
         cv2.imshow('object_detect', frame)
-        # cv2.imshow('gray',gray)
+        
 
         # we have suppose 24 fps then we have to run the loop for 40 ms
         if cv2.waitKey(40) == 27:
