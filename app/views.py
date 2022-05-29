51b780093b716556c7222a3c56feab446b6cddc0
RitamCODE <82175279+RitamCODE@users.noreply.github.com>
2022-05-29 23:05:03 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -11,35 +11,24 @@ from read_data import reading2
 
 UPLOAD_FOLDER = 'static/uploads'
 
+# to render the base tenmplate for the basic structuring of the website
 def base():
     return render_template("base.html")
 
+# to render the home page
 def index():
     return render_template("index.html")
 
+# to render the registration page
 def register():
     val = pipeline_model3()
     return render_template("register.html",value=val)
 
+# to render the faceapp page which authicates us using face recognition
 def faceapp():
     return render_template("faceapp.html")
 
-#def getwidth(path):
- #  size=img.size # returns the width and height of the image
-  #  aspect = size[0]/size[1] # width/height is the aspect ratio
-   # w=300*aspect
-    #return int(w)
-
-#def gender():
-    #   if request.method == 'POST':
-    #   f = request.files["image"]
-    #   filename = f.filename
-    #   path = os.path.join(UPLOAD_FOLDER,filename)
-    #    f.save(path)
-    #    w=getwidth(path)
-    #       return render_template("gender.html",fileupload=True,img_name=filename,w=w)
-#    return render_template("gender.html",fileupload=False,img_name="freeai.png",w=300)
-
+# to render the page which helps us to add data to the database
 def addSites():
     if request.method == 'POST':
         faceuserid=request.form["faceuserid"]
@@ -51,11 +40,13 @@ def addSites():
         return render_template("login.html",n=name,p=password,w=website)
     return render_template("addSites.html")
 
+# to render the face comparison page
 def compare():
     img=pipeline_model()
     ret=pipeline_model2()
     return render_template("compare.html",image=img,value=ret)
 
+# to render the page which fetches the stored data for a particular user
 def fetchSite():
     if request.method == 'POST':
         faceuserid = request.form["faceuserid"]
